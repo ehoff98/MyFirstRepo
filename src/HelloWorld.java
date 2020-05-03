fa01db2cf984c2a6cd06c96bde47021375c9e245
Erica Hoffman <hoffmae6@miamioh.edu>
2020-05-03 18:37:57 -0400

    My first commit in Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This is my first class for GitHub Testing
+ * @author hoffmae6
+ * @section C
+ * @date 3-3-2020
+ * @version version 1.0
+ */
+public class HelloWorld {
+ public static void main(String[] args) {
+ System.out.println("Hello World");
+ }
+}
